a32368b3421b0806975e3062508d41de12ee66f3
Sharad <sharadg@snapwork.com>
2025-06-25 08:09:16 +0530

    single commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'sqaush test',
+      title: 'single commit test 1.1',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
