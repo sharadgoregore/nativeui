30ffbd34874f2d6186584deba391b1820bed3330
Sharad <sharadg@snapwork.com>
2025-06-25 07:52:57 +0530

    initial commit to sharad branch

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demos',
+      title: 'sqaush test',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        useMaterial3: false,
       ),
       home:  NativeUi(),
     );
